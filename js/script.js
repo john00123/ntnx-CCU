5cffef1fa9ad635cb707ccf61a5021b1929acc66
John Torres <john.torres.hurtado@gmail.com>
2017-10-13 15:27:58 -0700

    added infomration

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const licenses = {
+  product:['Acropolis License', 'Software Encryption', 'File Server'],
+
+  aos:['Trial','Starter','Pro','Ultimate'],
+
+  expiration:['12000 CCU Expires 12/12/2020', '3 Licenses Expires 12/12/2022', '3 Licenses Expures 2023'],
+
+  
+}
