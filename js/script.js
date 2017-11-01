96880b06cd4b90ee05389c595f1962552a636277
John Torres <john.torres.hurtado@gmail.com>
2017-11-01 16:32:34 -0700

    change counter timing

--- js/script.js
+++ js/script.js
@@ -214,13 +214,13 @@ function countNumbers(){
     $(this).prop('Counter',0).animate({
         Counter: $(this).text() },
         {
-        duration: 2000,
+        duration: 1200,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
         }
     });
-    
+
   });
 
   setTimeout(function(){
