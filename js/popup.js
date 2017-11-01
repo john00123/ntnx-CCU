0bfae69250b45f3a3da93fd5bfdef6f87114aa96
John Torres <john.torres.hurtado@gmail.com>
2017-11-01 15:45:41 -0700

    added counter

--- js/popup.js
+++ js/popup.js
@@ -155,6 +155,7 @@ const layer2Data = {
 
         <div class='panel2'>
           <h3 mar> CCUs required</h3>
+
           <div class=flexbox-stretch>
             <kbd>${licensePrices[0].key}</kbd> <code>${licensePrices[0].price}</code>
           </div>
@@ -183,7 +184,7 @@ const layer2Data = {
 
 `
   <div class="popup-header">${popupData.title[3]}</div>
-  <div class="popup-body"> <h3> You have reclaimed all the unused CCUs available on this cluster </h3></div>
+  <div class="popup-body"> <h3> You have reclaimed all the unused CCUs available on this cluster </h3><span class='count money'>000</span></div>
   <div class="popup-footer">${popupData.footer[4]}</div>
 `,
 
